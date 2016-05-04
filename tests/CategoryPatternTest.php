57127d32a658b2288cbe25d7cdd65c25b042085e
tbf.caio <caio.nardi@tbfconsultoria.com.br>
2016-05-04 19:15:09 -0300

    Teste de cadastro de pattern

---
+++ tests/CategoryPatternTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CategoryPatternTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+    	$c = new \App\Model\Category();
+    	$c->name = 'Teste';
+    	$c->save();
+
+        $categoryPattern = new \App\Model\CategoryPattern();
+        $categoryPattern->description = 'Teste';
+        $categoryPattern->id_category = $c->id;
+        $categoryPattern->save();
+    }
+}

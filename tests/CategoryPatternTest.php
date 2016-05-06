d05451d49e56948fff1927f0a15dedee607e96f7
tbf.caio <caio.nardi@tbfconsultoria.com.br>
2016-05-06 17:47:31 -0300

    Teste do padrão

--- tests/CategoryPatternTest.php
+++ tests/CategoryPatternTest.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use \App\Model\CategoryPattern;
+use \App\Model\Category;
+
 class CategoryPatternTest extends TestCase
 {
     /**
@@ -13,13 +16,28 @@ class CategoryPatternTest extends TestCase
      */
     public function testInicial()
     {
-    	$c = new \App\Model\Category();
+        echo "\nini";
+    	$c = new Category();
     	$c->name = 'Teste';
     	$c->save();
 
-        $categoryPattern = new \App\Model\CategoryPattern();
+        $categoryPattern = new CategoryPattern();
         $categoryPattern->description = 'Teste';
+        $categoryPattern->pattern = '/^Teste.*/';
         $categoryPattern->id_category = $c->id;
-        $categoryPattern->save();
+        //$categoryPattern->save();
+    }
+
+    public function testMatch()
+    {
+        echo "\nlist";
+        $patterns = CategoryPattern::all();
+        $p1 = CategoryPattern::testPattern('TesteX');
+        $p2 = CategoryPattern::testPattern('Teste');
+        $p3 = CategoryPattern::testPattern('WEUY');
+
+        $this->assertNotNull($p1);
+        $this->assertNotNull($p2);
+        $this->assertFalse($p3);
     }
 }

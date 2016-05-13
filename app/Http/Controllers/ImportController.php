b29143c6b12760fc51a13127bdebbf14398ca2c1
tbf.caio <caio.nardi@tbfconsultoria.com.br>
2016-05-13 13:37:38 -0300

    Controller importando arquivo

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -2,15 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Input;
+use Validator;
+
+
+use \App\Model\Category;
+use \App\Model\Transaction;
+use \OfxParser\Ofx;
 
 class ImportController extends Controller
 {
     //
 
-    public function index(){
+    public function index()
+    {
     	return view('import');
     }
+
+    public function store()
+    {
+    	$image = Input::file('image');
+       /** $validator = Validator::make([$image], ['image' => 'required']);
+        if ($validator->fails()) {
+            return $this->errors(['message' => 'Not an image.', 'code' => 400]);
+        }***/
+        $destinationPath = storage_path() . '/uploads';
+        if(!$image->move($destinationPath, $image->getClientOriginalName())) {
+            return $this->errors(['message' => 'Error saving the file.', 'code' => 400]);
+        }
+
+        $ofxdata = storage_path() . '/uploads/'.$image->getClientOriginalName();
+        $OfxParser = new \OfxParser\Parser;
+        $Ofx = $OfxParser->loadFromFile($ofxdata);
+        $Account = $Ofx->BankAccount;
+        $Statement = $Account->Statement;
+        $Transactions = $Statement->transactions;
+        foreach( $Transactions as $i => $transaction ){
+            $result = Transaction::testTransaction($transaction);
+        }
+        return response()->json(['success' => true], 200);
+    }
+
 }
--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -1,2 +1,26 @@
 @extends('layout.master')
 @section('title', 'Importar arquivo')
+
+@section('content')
+	<div class="about-section">
+		<div class="text-content">
+			<div class="span7 offset1">
+				@if(Session::has('success'))
+					<div class="alert-box success">
+						<h2>{!! Session::get('success') !!}</h2>
+					</div>
+				@endif
+				<div class="secure">Envie o OFX</div>
+					{{Form::open(array('url'=>'import','method'=>'POST', 'files'=>true))}}
+				<div class="control-group">
+					<div class="controls">
+					{{Form::file('file')}}	
+					</div>
+				</div>
+				<div id="success"> </div>
+				{{Form::submit('Enviar', array('class'=>'send-btn'))}}
+				{{Form::close()}}
+			</div>
+		</div>
+	</div>
+@endsection

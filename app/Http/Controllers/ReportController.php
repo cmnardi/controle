be524d18bd2f4414c2826f9e1e96f538cfcf3282
Caio Marcelo Nardi <caio.nardi@tbfconsultoria.com.br>
2016-06-10 23:54:45 -0300

    Gráfico - renomeando o arquivo .blade

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,7 +11,7 @@ class ReportController extends Controller
     //
     public function index()
     {
-        return view('grafico');
+        return view('graph');
     }
 
     public function data()
